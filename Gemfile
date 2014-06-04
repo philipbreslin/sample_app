1e4b8221c5ed4792c4543c8c65ca80cf2e0ac959
Philip Breslin <philipbreslin@hotmail.com>
2014-06-05 00:33:12 +0100

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -45,6 +45,9 @@ end
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt-ruby', ' 3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use unicorn as the app server
 # gem 'unicorn'
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,14 @@
 include ApplicationHelper
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		# Sign in when not using Cappybara.
+		remember_token = User.new_remember_token
+		cookies[:remember_token] = remember_token
+		user.update_attribute(:remember_token, User.digest(remember_token))
+	else
+		visit signin_path
+		fill_in "Email", with: user.email
+		click_button "Sign in"
+	end
+end
